b003f67cdb821a9212bea00896f6a9320a9e16c8
Ruslan <plotkin1987@gmail.com>
2020-08-24 10:14:30 +0300

    fix-7

--- src/app/home/stories/stories.component.ts
+++ src/app/home/stories/stories.component.ts
@@ -9,7 +9,23 @@ export class StoriesComponent implements OnInit {
 
   constructor() { }
 
-  
+  stories: any[] = [
+    {
+      name: "Jeff Jons",
+      job: "Businessman",
+      comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown"
+    },
+    {
+      name: "Linda Klain",
+      job: "Journalist",
+      comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown"
+    },
+    {
+      name: "Samanta Folil",
+      job: "Reporter",
+      comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown"
+    }
+  ];
 
   
 
